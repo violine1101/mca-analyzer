008250ab197a82a23a57fed1007c1f8a88ae4f12
violine1101 <violine1101@gmail.com>
2021-06-24 19:17:16 +0200

    Add chunk unloading to ChunkLoader

--- src/chunk_loader.rs
+++ src/chunk_loader.rs
@@ -1,20 +1,17 @@
-#![allow(unused)]
-// TODO ^^^
-
-use std::collections::{hash_map::Entry, HashMap};
+use std::collections::{hash_map::Entry, HashMap, VecDeque};
 
 use anvil_region::{
     position::{RegionChunkPosition, RegionPosition},
     provider::{FolderRegionProvider, RegionProvider},
 };
 
-use crate::{
-    chunk::Chunk,
-    chunk_section::{ChunkSectionBlock, CHUNK_SIZE},
-};
+use crate::{chunk::Chunk, chunk_section::CHUNK_SIZE};
+
+const MAX_LOADED_CHUNKS: usize = 32;
 
 pub struct ChunkLoader<'a> {
     loaded_chunks: HashMap<(i32, i32), Chunk>,
+    recently_loaded_chunks: VecDeque<(i32, i32)>,
     region_provider: FolderRegionProvider<'a>,
 }
 
@@ -22,11 +19,36 @@ impl<'a> ChunkLoader<'a> {
     pub fn new(region_folder: &'a str) -> Self {
         ChunkLoader {
             loaded_chunks: HashMap::new(),
+            recently_loaded_chunks: VecDeque::new(),
             region_provider: FolderRegionProvider::new(region_folder),
         }
     }
 
+    fn load_chunk(&mut self, coordinate: (i32, i32)) {
+        if let Some(index) = self
+            .recently_loaded_chunks
+            .iter()
+            .position(|&el| el == coordinate)
+        {
+            self.recently_loaded_chunks.remove(index);
+            self.recently_loaded_chunks.push_back(coordinate);
+        }
+    }
+
+    fn unload_chunks(&mut self) {
+        if self.recently_loaded_chunks.len() >= MAX_LOADED_CHUNKS {
+            for _ in 0..(self.recently_loaded_chunks.len() - MAX_LOADED_CHUNKS) {
+                if let Some(least_recently_loaded_chunk) = self.recently_loaded_chunks.pop_front() {
+                    self.loaded_chunks.remove(&least_recently_loaded_chunk);
+                }
+            }
+        }
+    }
+
     pub fn get_or_load(&mut self, chunk_x: i32, chunk_z: i32) -> &Chunk {
+        self.load_chunk((chunk_x, chunk_z));
+        self.unload_chunks();
+
         match self.loaded_chunks.entry((chunk_x, chunk_z)) {
             Entry::Occupied(entry) => entry.into_mut(),
             Entry::Vacant(entry) => {
